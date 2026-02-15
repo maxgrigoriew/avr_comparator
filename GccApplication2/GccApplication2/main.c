afbe724b83afcf292593ab1b4a65b49738bb733b
maksim.khlopyanik <maksim.khlopyanik@ural.rt.ru>
2026-02-15 21:11:32 +0300

    добавлено сравление с внутренним компаратором

---
+++ GccApplication2/GccApplication2/main.c
@@ -0,0 +1,23 @@
+#include <avr/io.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
+ 
+
+ 
+
+int main(void) {
+    DDRD |= (1<<PD0);        // PD0 = PWM выход
+    DDRD &= ~(1<<PD6) | (1 << PD7);  
+
+	ACSR &= ~(1 << ACD); // Включаем компаратор
+	ACSR |= (1 << ACBG); // Внешнее сравнение с землей
+
+
+    while(1) {
+       if (ACSR & (1 << ACO)) { // Если компаратор сработал
+		   PORTD |= (1 << PD0); // Включаем светодиод
+	   } else {
+		   PORTD &= ~(1 << PD0); // Выключаем светодиод
+	   }
+    }
+}
