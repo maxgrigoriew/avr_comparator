58566ed5b58989f706859902795164efaa35f7b5
maksim.khlopyanik <maksim.khlopyanik@ural.rt.ru>
2026-04-21 23:37:25 +0300

    минималья версия работы компаратора

--- GccApplication2/GccApplication2/main.c
+++ GccApplication2/GccApplication2/main.c
@@ -1,22 +1,20 @@
 #include <avr/io.h>
 #include <util/delay.h>
 #include <avr/interrupt.h>
- 
+
 #define F_CPU 8000000UL
+
  
 int main(void) {
-	
-	ACSR &= ~(1 << ACD); // Включаем компаратор
-	ACSR |= (1 << ACIE); // Разрешаем прерывание от компаратора
+    DDRD |= (1 << PD0);
 
-	ACSR |= (1 << ACBG); // Внутренний компаратор
-	DDRD |= (1 << 0); // Устанавливаем PD0 как выход для светодиода
+    ACSR |= (1 << ACBG);
 
-    while(1) {
-       if (ACSR & (1 << ACO)) { // Если компаратор сработал AIN0 > AIN1
-		PORTD &= ~(1 << PD0); // Выключаем светодиод
-	   } else {
-		   PORTD |= (1 << PD0); // Включаем светодиод
-	   }
+    while (1) {
+        if (ACSR & (1 << ACO)) {
+            PORTD &= ~(1 << PD0);
+        } else {
+            PORTD |= (1 << PD0);
+        }
     }
 }
